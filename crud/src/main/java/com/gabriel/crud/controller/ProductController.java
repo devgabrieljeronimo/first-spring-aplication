4040f0d8f44a582eeec84520877fe4b4b0c84795
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-07-09 19:32:56 -0300

    Add class ProductController

---
+++ crud/src/main/java/com/gabriel/crud/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.gabriel.crud.controller;
+
+import com.gabriel.crud.model.Product;
+import com.gabriel.crud.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping
+    public ResponseEntity getAll() {
+        List<Product> productList = productRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+}
