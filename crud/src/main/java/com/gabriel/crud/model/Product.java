11e74b5cba044245b2ed1c71f7397c2e1118808d
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-07-09 19:04:01 -0300

    Upgrade entity class Product

---
+++ crud/src/main/java/com/gabriel/crud/model/Product.java
@@ -0,0 +1,46 @@
+package com.gabriel.crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity(name = "product")
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private Long price;
+
+    public Product() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
