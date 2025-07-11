8e0bf1ff26439964cfc48c70e10c59aa7ad5210a
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-07-11 20:08:55 -0300

    Modify method save to support dto class

--- crud/src/main/java/com/gabriel/crud/controller/ProductController.java
+++ crud/src/main/java/com/gabriel/crud/controller/ProductController.java
@@ -1,13 +1,13 @@
 package com.gabriel.crud.controller;
 
+import com.gabriel.crud.dtos.ProductDto;
 import com.gabriel.crud.model.Product;
 import com.gabriel.crud.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +16,18 @@ import java.util.List;
 public class ProductController {
 
     @Autowired
-    ProductRepository productRepository;
+    ProductRepository repository;
 
     @GetMapping
     public ResponseEntity getAll() {
-        List<Product> productList = productRepository.findAll();
+        List<Product> productList = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ProductDto dto) {
+        var product = new Product();
+        BeanUtils.copyProperties(dto, product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
     }
 }
